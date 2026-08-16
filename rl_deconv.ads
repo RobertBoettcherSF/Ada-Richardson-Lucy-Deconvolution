b437970a9d1e350aba5bc73c280e890b265ddc07
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-08-16 12:09:19 +0200

    Remove parameter documentation from Deconvolve function
    Removed parameter documentation comments from the Deconvolve function.

--- rl_deconv.ads
+++ rl_deconv.ads
@@ -1,4 +1,3 @@
--- rl_deconv.ads
 package RL_Deconvolution is
    -- Custom types for the algorithm
    type Matrix is array (Integer range <>, Integer range <>) of Float;
@@ -8,11 +7,6 @@ package RL_Deconvolution is
    Zero_Division      : exception;
 
    -- Main Richardson-Lucy Deconvolution Procedure
-   -- @param Observed: The blurred input image
-   -- @param PSF: Point Spread Function (kernel)
-   -- @param Iterations: Number of iterations to run
-   -- @param Damped: If True, uses dampened variant (prevents noise amplification)
-   -- @return: The restored image
    function Deconvolve (
       Observed   : Matrix;
       PSF        : Matrix;
